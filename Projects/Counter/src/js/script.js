c9a385208f59a7300210629d2257b22bce89a5d2
Davi Luiz <252206266+DaviAjax@users.noreply.github.com>
2026-01-05 16:15:07 -0400

    Initial commit

---
+++ Projects/Counter/src/js/script.js
@@ -0,0 +1,29 @@
+const countElement = document.getElementById("counter");
+const incrementInput = document.getElementById("increment");
+
+const btnSubtract = document.getElementById("btnSubtract");
+const btnReset = document.getElementById("btnReset");
+const btnSum = document.getElementById("btnSum");
+
+let counter = 0;
+
+function updateCounter() {
+  countElement.textContent = counter;
+}
+
+btnSum.addEventListener("click", () => {
+  const valor = Number(incrementInput.value);
+  counter += valor;
+  updateCounter();
+});
+
+btnSubtract.addEventListener("click", () => {
+  const valor = Number(incrementInput.value);
+  counter -= valor;
+  updateCounter();
+});
+
+btnReset.addEventListener("click", () => {
+  counter = 0;
+  updateCounter();
+});
